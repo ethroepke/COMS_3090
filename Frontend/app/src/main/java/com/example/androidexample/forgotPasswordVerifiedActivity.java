2f1409ad25fc14f789438bb82e58316d1e7fd9c3
eroepke <164393462+ethroepke@users.noreply.github.com>
2024-10-03 14:58:32 -0500

    updates

---
+++ Frontend/app/src/main/java/com/example/androidexample/forgotPasswordVerifiedActivity.java
@@ -0,0 +1,21 @@
+package com.example.androidexample;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class forgotPasswordVerifiedActivity extends AppCompatActivity {
+
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.forgotpasswordverified);
+    }
+}
